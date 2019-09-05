2e66d151c44e59b5269270b648bea582704dc8e6
潘慧娟 <pan_huij@163.com>
2019-09-05 20:52:54 +0800

    合并完成

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import * as port from "./port/index"
 import request from "../utils/request"
 
@@ -19,34 +19,10 @@ export let getExamType = ()=>{
   return request.get(port.getExamType);
 }
 
-//  获取题目类型
-export let getQuestionsType = ()=>{
-  return request.get(port.getQuestionsType);
-}
 
 // 添加试题类型
 export let insertQuestionsType = (params: object)=>{
   return request.get(port.insertQuestionsType,{params});
-}
-=======
-import * as port from "./port/index"
-import request from "../utils/request"
-
-
-
-// 获取试题
-export let getQuestion = (params: object)=>{
-  return request.get(port.getQuestion, {params});
-}
-
-// 获取课程安排
-export let getSubject = ()=>{
-  return request.get(port.getSubject);
-}
-
-// 获取考试类型
-export let getExamType = ()=>{  
-  return request.get(port.getExamType);
 }
 
 // 获取考试类型
@@ -68,4 +44,3 @@ export let getmangerroom = ()=>{
 export let getmangerstudentnew = ()=>{
   return request.get(port.getmangerstudentnew);
 }
->>>>>>> 75de82662435c15033062cbce8cb24df30c0c742
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
-import User from "./modules/user/user"
-import Question from "./modules/question/question"
-import Watchquestions from "./modules/question/watchquestions"
-
-const user=new User()
-const question=new Question()
-const watchquestions=new Watchquestions()
-
-export default {
-  user,
-  question,
-  watchquestions
-}
-=======
 import User from "./modules/user/user"
 import Question from "./modules/question/question"
 import Watchquestions from "./modules/question/watchquestions"
 import Grade  from './modules/classManagement/grade'
 import Room from './modules/classManagement/room'
 import Student from './modules/classManagement/student'
-
-
 
 const user=new User()
 const question=new Question()
@@ -36,5 +19,4 @@ export default {
   grade,
   room,
   student
-} 
->>>>>>> 75de82662435c15033062cbce8cb24df30c0c742
+}
